258e5cf98cf37d5a717472a508c0660c99b187b8
Glenn Manguiat <manguiatgkd@aub.com.ph>
2025-05-26 15:01:20 +0800

    Initial Commit

---
+++ Resilience/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Resilience
+//
+//  Created by Glenn Karlo Manguiat on 5/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Resilience/ResilienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResilienceApp.swift
+//  Resilience
+//
+//  Created by Glenn Karlo Manguiat on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ResilienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
